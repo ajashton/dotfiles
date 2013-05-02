e1dee3c1692094eb22d30271b3f8a9ac9a8ca18b
AJ Ashton <aj.ashton@gmail.com>
2013-05-02 15:56:37 -0400

    gsettings interface tweaks

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -43,8 +43,13 @@ curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 nvm install 0.10
 
 # gnome settings
-gsettings set org.gnome.settings-daemon.peripherals.touchpad horiz-scroll-enabled true
-gsettings set org.gnome.settings-daemon.peripherals.touchpad natural-scroll true
-gsettings set org.gnome.settings-daemon.peripherals.touchpad scroll-method two-finger-scrolling
-gsettings set org.gnome.settings-daemon.peripherals.touchpad tap-to-click false
+gs='gsettings set'
+$gs org.gnome.settings-daemon.peripherals.touchpad  horiz-scroll-enabled    true
+$gs org.gnome.settings-daemon.peripherals.touchpad  natural-scroll          true
+$gs org.gnome.settings-daemon.peripherals.touchpad  scroll-method           two-finger-scrolling
+$gs org.gnome.settings-daemon.peripherals.touchpad  tap-to-click            false
+$gs org.gnome.desktop.interface                     font-name               'Open Sans 10'
+$gs org.gnome.desktop.media-handling                automount-open          false
+$gs org.gnome.desktop.wm.preferences                mouse-button-modifier   '<Mod4>'
+$gs org.gnome.desktop.wm.preferences                titlebar-font           'Open Sans Bold 9'
 
