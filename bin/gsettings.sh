47fe51ad7e0c01b0402858c29b8226eeae71d6d6
AJ Ashton <aj.ashton@gmail.com>
2013-08-24 21:57:33 -0400

    disable screensaver screen lock

--- bin/gsettings.sh
+++ bin/gsettings.sh
@@ -8,6 +8,7 @@ $gs org.gnome.settings-daemon.peripherals.touchpad  scroll-method           two-
 $gs org.gnome.settings-daemon.peripherals.touchpad  tap-to-click            false
 #$gs org.gnome.desktop.interface                     font-name               'Open Sans 10'
 $gs org.gnome.desktop.media-handling                automount-open          false
+$gs org.gnome.desktop.screensaver                   lock-enabled            false
 $gs org.gnome.desktop.wm.preferences                mouse-button-modifier   '<Mod4>'
 #$gs org.gnome.desktop.wm.preferences                titlebar-font           'Open Sans Bold 9'
 $gs org.gnome.nautilus.preferences                  enable-delete           true
