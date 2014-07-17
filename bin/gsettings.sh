5653e5af43faa34b5a221d5a41b1833c2647a38b
AJ Ashton <aj.ashton@gmail.com>
2014-07-16 22:29:55 -0400

    gnome settings changes

--- bin/gsettings.sh
+++ bin/gsettings.sh
@@ -6,11 +6,12 @@ $gs org.gnome.settings-daemon.peripherals.touchpad  horiz-scroll-enabled    true
 $gs org.gnome.settings-daemon.peripherals.touchpad  natural-scroll          true
 $gs org.gnome.settings-daemon.peripherals.touchpad  scroll-method           two-finger-scrolling
 $gs org.gnome.settings-daemon.peripherals.touchpad  tap-to-click            false
-$gs org.gnome.desktop.interface                     font-name               'Roboto 10'
-$gs org.gnome.desktop.interface                     icon-theme              'Moka'
+# prevents xmodmap from being overridden:
+$gs org.gnome.settings-daemon.plugins.keyboard      active                  false
+$gs org.gnome.desktop.interface                     font-name               'Arimo 10'
 $gs org.gnome.desktop.media-handling                automount-open          false
 $gs org.gnome.desktop.screensaver                   lock-enabled            false
 $gs org.gnome.desktop.wm.preferences                mouse-button-modifier   '<Mod4>'
-$gs org.gnome.desktop.wm.preferences                titlebar-font           'Roboto 10'
+$gs org.gnome.desktop.wm.preferences                titlebar-font           'Arimo Bold 10'
 $gs org.gnome.nautilus.preferences                  enable-delete           true
 $gs org.gnome.shell.overrides                       button-layout           'close,maximize:'
