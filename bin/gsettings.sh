0efda3ee14bc40b7267ec9036703b859d55efc9f
AJ Ashton <aj.ashton@gmail.com>
2017-05-03 15:56:18 -0400

    disable gnome-settings xrandr plugin

--- bin/gsettings.sh
+++ bin/gsettings.sh
@@ -8,10 +8,10 @@ $gs org.gnome.settings-daemon.peripherals.touchpad  scroll-method           two-
 $gs org.gnome.settings-daemon.peripherals.touchpad  tap-to-click            false
 # prevents xmodmap from being overridden:
 $gs org.gnome.settings-daemon.plugins.keyboard      active                  false
-$gs org.gnome.desktop.interface                     font-name               'Roboto 10'
+# prevents Mod4+P from switching displays:
+$gs org.gnome.settings-daemon.plugins.xrandr        active                  false
 $gs org.gnome.desktop.media-handling                automount-open          false
 $gs org.gnome.desktop.screensaver                   lock-enabled            false
 $gs org.gnome.desktop.wm.preferences                mouse-button-modifier   '<Mod4>'
-$gs org.gnome.desktop.wm.preferences                titlebar-font           'Roboto Bold 10'
 $gs org.gnome.nautilus.preferences                  enable-delete           true
 $gs org.gnome.shell.overrides                       button-layout           'close,maximize:'
