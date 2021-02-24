91d35e26c2db65afe141c937de9c787b1c9d9e9e
AJ Ashton <aj.ashton@gmail.com>
2021-02-24 13:09:40 -0500

    catch potential unzip error

--- .local/bin/import_album.sh
+++ .local/bin/import_album.sh
@@ -9,13 +9,22 @@ function cleanup() {
 }
 trap cleanup EXIT
 
+function continue_prompt() {
+  cmd="${1:-Previous command}"
+  read -r -p "$cmd may have failed. Continue anyway? [y/N]: " response
+  case "$response" in
+    [yY][eE][sS]|[yY]) true;;
+    *) exit 1;
+  esac
+}
+
 function main() {
 
   zipfile="$(readlink -f "$1")"
 
   pushd "$tmp_dir" > /dev/null
 
-  unzip -j "$zipfile"
+  unzip -j "$zipfile" || continue_prompt Unzip
 
   eyeD3 --plugin=fixup \
     --dir-rename-pattern="$(basename "$tmp_dir")" \
