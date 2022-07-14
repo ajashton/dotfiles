eeeff2ee3a39b65aafe7ceb3370404e2cd27f86d
AJ Ashton <aj.ashton@gmail.com>
2022-07-14 13:59:36 -0400

    import_album.sh improvements
    - Now works with folders in addition to zip files
    - Adjusted file naming convention
    - Now looks for *.m4a & *.ogg in addition to *.mp3

--- .local/bin/import_album.sh
+++ .local/bin/import_album.sh
@@ -20,20 +20,26 @@ function continue_prompt() {
 
 function main() {
 
-  zipfile="$(readlink -f "$1")"
+  if [[ "$1" =~ *.zip ]]; then
+    zipfile="$(readlink -f "$1")"
+    unzip -j "$zipfile" -d "$tmp_dir" || continue_prompt Unzip
+  elif [[ -d "$1" ]]; then
+    cp -rp "$1"/* "$tmp_dir"/
+  else
+    echo "ERROR: unexpected input"
+    exit 1
+  fi
 
   pushd "$tmp_dir" > /dev/null
 
-  unzip -j "$zipfile" || continue_prompt Unzip
-
   eyeD3 --plugin=fixup \
     --dir-rename-pattern="$(basename "$tmp_dir")" \
-    --file-rename-pattern='$album_artist@@@$album ($best_date:year)@@@$track:num. $title' \
+    --file-rename-pattern='$album_artist@@@$album@@@$track:num. $title' \
     .
 
   echo "$?"
 
-  for f in *.mp3; do
+  for f in *.mp3 *.m4a *.ogg; do
     mkdir -p ~/Music/Albums/"$(dirname "${f//@@@/\/}")"
     mv "$f" ~/Music/Albums/"${f//@@@/\/}"
     mv cover.* ~/Music/Albums/"$(dirname "${f//@@@/\/}")" || true
